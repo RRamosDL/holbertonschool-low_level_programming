6914b3914d1699714b4dcb22f0b8727b2549cf3b
hermeneuta@gmail.com <hermeneuta@gmail.com>
2023-06-20 07:06:24 -0700

    task 0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -10,12 +10,12 @@
 
 int main(void)
 {
-	char text[9] = "_putchar";
+	char text[8] = "_putchar";
 	int i = 0;
 
 	for (i = 0; i < 9; i++)
 	{
-	_putchar(text[i]);
+		_putchar(text[i]);
 	}
 	_putchar ('\n');
 
